f247eca05fb7e450d099ccfb67ca4e95f7036699
iuri aranda <iuri.aranda@gmail.com>
2023-09-05 18:05:02 +0200

    Initial implementation of the bitbucket runners (#1)
    * Initial implementation of the bitbucket runners
    
    * Set version requirements
    
    Co-authored-by: Philip Dubois <hello@philipdubois.be>
    
    ---------
    
    Co-authored-by: Philip Dubois <hello@philipdubois.be>

---
+++ service.tf
@@ -0,0 +1,24 @@
+resource "kubernetes_service" "bitbucket_runner" {
+  for_each = var.bitbucket_runners
+
+  metadata {
+    name      = "bitbucket-${each.key}"
+    namespace = var.k8s_namespace
+
+    labels = merge(local.k8s_default_labels, {
+      "app.kubernetes.io/name"     = "bitbucket-runner"
+      "app.kubernetes.io/instance" = each.key
+    })
+  }
+
+  spec {
+    selector = {
+      "app.kubernetes.io/name"     = "bitbucket-runner"
+      "app.kubernetes.io/instance" = each.key
+    }
+
+    port {
+      port = 9999
+    }
+  }
+}
